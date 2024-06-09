7d743c17788451cd73a2be7741d7972fc3c81b5c
lucia-ensenat <gradolucia2022@gmail.com>
2024-06-09 18:31:49 +0200

    RESOLVED - añadir evento + unirte al calendario

--- app/src/main/java/com/lucia/Freet/ui/form/CalendarForm.java
+++ app/src/main/java/com/lucia/Freet/ui/form/CalendarForm.java
@@ -1,25 +1,40 @@
 package com.lucia.Freet.ui.form;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.lucia.Freet.Home;
 import com.lucia.Freet.Profile;
 import com.lucia.Freet.R;
+import com.lucia.Freet.services.ConnectionService;
+import com.lucia.Freet.utils.SharedPreferencesUtils;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Random;
 import java.util.UUID;
 
 public class CalendarForm extends AppCompatActivity {
     private EditText nameEditText;
+    private ConnectionService connectionService;
     private BottomNavigationView navigationView;
     private Switch share;
 
@@ -30,15 +45,24 @@ public class CalendarForm extends AppCompatActivity {
 
         nameEditText = findViewById(R.id.nameEditText);
         share = findViewById(R.id.switchShare);
-        final String uuid = UUID.randomUUID().toString();
+        navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setSelectedItemId(R.id.addCalendar);
+        final Button saveButton = findViewById(R.id.button);
+        saveButton.setEnabled(false);
+        final Button buttonCalendar = findViewById(R.id.buttonCalenario);
+        connectionService = new ConnectionService();
+        Random rand = new Random();
+
+        final String id =  String.valueOf(rand.nextInt((9999 - 0) + 1) + 0);
 
         share.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(CalendarForm.this);
-                    builder.setTitle(String.format("Comparte el código : %s", uuid));
-                    builder.setMessage("Comparte el código con tus amigos \n para que tengan acceso al calendario \n Luego podrás volver a consultarlo, no te preocupes");
+                    builder.setTitle(String.format("Comparte el código : %s", id));
+                    builder.setMessage("Comparte el código con tus amigos \n para que tengan acceso al calendario \n No lo podrás volver a consultar, asegurate de que lo guardas bine ");
                     builder.setCancelable(true);
+                    builder.show();
 
                 }
 
@@ -46,8 +70,21 @@ public class CalendarForm extends AppCompatActivity {
             }
         });
 
-        navigationView =  findViewById(R.id.bottom_navigation);
-        navigationView.setSelectedItemId(R.id.addCalendar);
+        nameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                saveButton.setEnabled(!s.toString().trim().isEmpty());
+            }
+        });
+
         navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -62,7 +99,7 @@ public class CalendarForm extends AppCompatActivity {
                 } else if (itemId == R.id.profile) {
                     intent = new Intent(getApplicationContext(), Profile.class);
                     startActivity(intent);
-                }else if (itemId == R.id.addCalendar) {
+                } else if (itemId == R.id.addCalendar) {
                     intent = new Intent(getApplicationContext(), CalendarForm.class);
                     startActivity(intent);
                 }
@@ -71,5 +108,132 @@ public class CalendarForm extends AppCompatActivity {
         });
 
 
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SaveCalendarTask saveCalendarTask = new SaveCalendarTask(nameEditText.getText().toString(), id);
+                saveCalendarTask.execute();
+            }
+        });
+
+
+        buttonCalendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(CalendarForm.this);
+                builder.setTitle("Introduce el código del calendario");
+                final EditText calendarCode = new EditText(CalendarForm.this);
+                calendarCode.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(calendarCode);
+
+                builder.setPositiveButton("UNIRTE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        NewUserToCalendarTask newUserToCalendarTask = new NewUserToCalendarTask(calendarCode.getText().toString());
+                        newUserToCalendarTask.execute();
+                    }
+                });
+
+                builder.setNegativeButton("CANCELAR", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.show();
+
+
+            }
+        });
+
     }
+
+    private class NewUserToCalendarTask extends AsyncTask<String, Void, Boolean> {
+        private String uuid;
+
+        public NewUserToCalendarTask(String uuid) {
+
+            this.uuid = uuid;
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            try (final Connection connection = connectionService.createConnection()) {
+                try (final PreparedStatement statement = connection.prepareStatement("INSERT INTO `tiene`(`nickname`, `idCalendario`) VALUES (?,?)")) {
+                    statement.setString(1, SharedPreferencesUtils.get("nickname", CalendarForm.this));
+                    statement.setString(2, this.uuid);
+                    int update = statement.executeUpdate();
+                    if (update > 0) {
+                        return true;
+                    }
+
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            if (success) {
+                Toast toast = Toast.makeText(CalendarForm.this, "Ya formas parte del calendario", Toast.LENGTH_SHORT);
+                toast.show();
+
+            } else {
+                Toast toast = Toast.makeText(CalendarForm.this, "No se ha podido llevar a cabo", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+
+    private class SaveCalendarTask extends AsyncTask<String, Void, Boolean> {
+        private String uuid;
+        private String name;
+
+        public SaveCalendarTask(String name, String uuid) {
+
+            this.uuid = uuid;
+            this.name = name;
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            try (final Connection connection = connectionService.createConnection()) {
+                try (final PreparedStatement statement = connection.prepareStatement("INSERT INTO `calendarios`(`id`, `calendario`) VALUES (?,?)")) {
+                    statement.setString(1, this.uuid);
+                    statement.setString(2, this.name);
+                    int update = statement.executeUpdate();
+                    if (update > 0) {
+                        return true;
+                    }
+
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            if (success) {
+                NewUserToCalendarTask newUserToCalendarTask = new NewUserToCalendarTask(this.uuid);
+                newUserToCalendarTask.execute();
+
+                Toast toast = Toast.makeText(CalendarForm.this, "Se ha creado el nuevo calendario", Toast.LENGTH_SHORT);
+                toast.show();
+
+
+            } else {
+                Toast toast = Toast.makeText(CalendarForm.this, "No se ha podido llevar a cabo", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+
+
 }
--- app/src/main/java/com/lucia/Freet/ui/form/EventForm.java
+++ app/src/main/java/com/lucia/Freet/ui/form/EventForm.java
@@ -3,6 +3,7 @@ package com.lucia.Freet.ui.form;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -10,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,6 +34,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -84,16 +87,18 @@ public class EventForm extends AppCompatActivity {
                 } else if (itemId == R.id.profile) {
                     intent = new Intent(getApplicationContext(), Profile.class);
                     startActivity(intent);
-                }else if (itemId == R.id.addCalendar) {
+                } else if (itemId == R.id.addCalendar) {
                     intent = new Intent(getApplicationContext(), CalendarForm.class);
                     startActivity(intent);
                 }
                 return false;
             }
         });
+        final List<Calendar> calendarList = new ArrayList<>();
 
-        List<Calendar> calendarList = calendarExtractor.extractUserCalendars(getApplicationContext());
-        updateCalendarsSpinner(calendarList);
+        ObtainCalendarTask obtainCalendarTask = new ObtainCalendarTask(calendarList);
+        obtainCalendarTask.execute();
+
 
         MaterialDatePicker<Pair<Long, Long>> pickerRange = MaterialDatePicker.Builder.dateRangePicker()//
                 .setTitleText("SELECT A DATE")//
@@ -141,7 +146,7 @@ public class EventForm extends AppCompatActivity {
             final java.util.Calendar startTimeStamp;
             final java.util.Calendar endTimeStamp;
 
-            if(startDate != null && endDate != null ) {
+            if (startDate != null && endDate != null) {
                 startTimeStamp = java.util.Calendar.getInstance();
                 startTimeStamp.setTime(startDate);
                 startTimeStamp.set(java.util.Calendar.HOUR_OF_DAY, startHours);
@@ -156,48 +161,11 @@ public class EventForm extends AppCompatActivity {
                 startTimeStamp = null;
             }
 
-            Calendar selectedCalendar = (Calendar) calendars.getSelectedItem();
-            try (final Connection connection = connectionService.createConnection()) {
+            final Calendar selectedCalendar = (Calendar) calendars.getSelectedItem();
 
-                try (final PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO `evento`(`fecha`, `fechaFin`, `evento`, `lugar`, `idCalendario`) VALUES (?,?,?,?,?)", PreparedStatement.RETURN_GENERATED_KEYS);//
-              final PreparedStatement obtainCalendarUsers = connection.prepareStatement("SELECT nickname FROM `tiene` WHERE `idCalendario` = ? ");//
-                     final PreparedStatement insertToCalendar = connection.prepareStatement("INSERT INTO asiste (`idEvento`,`nickname`) VALUES (?,?)")) {
+            final InsertEventTask insertEventTask = new InsertEventTask(selectedCalendar, startTimeStamp, endTimeStamp);
+            insertEventTask.execute();
 
-                    java.sql.Date sqlStartDate = new java.sql.Date(startTimeStamp.getTimeInMillis());
-                    java.sql.Date sqlEndDate = new java.sql.Date(endTimeStamp.getTimeInMillis());
-
-                    preparedStatement.setDate(1, sqlStartDate);
-                    preparedStatement.setDate(2, sqlEndDate);
-                    preparedStatement.setString(3, eventName.getText().toString());
-                    preparedStatement.setString(4, eventPlace.getText().toString());
-                    preparedStatement.setString(5, selectedCalendar.getUUID());
-                    preparedStatement.executeUpdate();
-
-                    ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
-                    if (generatedKeys.next()) {
-                        int eventoId = generatedKeys.getInt(1);
-
-
-
-                           obtainCalendarUsers.setString(1, selectedCalendar.getUUID());
-                           try (final ResultSet rsCalendar = obtainCalendarUsers.executeQuery()) {
-                               while (rsCalendar.next()) {
-                                   insertToCalendar.setInt(1, eventoId);
-                                   insertToCalendar.setString(2, rsCalendar.getString("nickname"));
-                                   insertToCalendar.executeUpdate();
-
-                               }
-
-                           }
-
-                       }
-                   }
-
-
-
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
         });
     }
 
@@ -231,5 +199,102 @@ public class EventForm extends AppCompatActivity {
         AlertDialog alertDialog = builder.create();
         // Show the Alert Dialog box
         alertDialog.show();
+    }
+
+
+    private class InsertEventTask extends AsyncTask<String, Void, Boolean> {
+        private Calendar calendar;
+        private java.util.Calendar startTime;
+        private java.util.Calendar endTime;
+
+        public InsertEventTask(Calendar calendar, java.util.Calendar startTime, java.util.Calendar endTime) {
+
+            this.calendar = calendar;
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            try (final Connection connection = connectionService.createConnection()) {
+
+                try (final PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO `evento`(`fecha`, `fechaFin`, `evento`, `lugar`, `idCalendario`) VALUES (?,?,?,?,?)", PreparedStatement.RETURN_GENERATED_KEYS);//
+                     final PreparedStatement obtainCalendarUsers = connection.prepareStatement("SELECT nickname FROM `tiene` WHERE `idCalendario` = ? ");//
+                     final PreparedStatement insertToCalendar = connection.prepareStatement("INSERT INTO asiste (`idEvento`,`nickname`) VALUES (?,?)")) {
+
+                    java.sql.Date sqlStartDate = new java.sql.Date(startTime.getTimeInMillis());
+                    java.sql.Date sqlEndDate = new java.sql.Date(endTime.getTimeInMillis());
+
+                    preparedStatement.setDate(1, sqlStartDate);
+                    preparedStatement.setDate(2, sqlEndDate);
+                    preparedStatement.setString(3, eventName.getText().toString());
+                    preparedStatement.setString(4, eventPlace.getText().toString());
+                    preparedStatement.setString(5, calendar.getUUID());
+                    preparedStatement.executeUpdate();
+
+                    ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+                    if (generatedKeys.next()) {
+                        int eventoId = generatedKeys.getInt(1);
+
+
+                        obtainCalendarUsers.setString(1, calendar.getUUID());
+                        try (final ResultSet rsCalendar = obtainCalendarUsers.executeQuery()) {
+                            while (rsCalendar.next()) {
+                                insertToCalendar.setInt(1, eventoId);
+                                insertToCalendar.setString(2, rsCalendar.getString("nickname"));
+                                insertToCalendar.executeUpdate();
+
+                            }
+
+                        }
+
+                    }
+                }
+
+                return true;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            Toast toast;
+            if (success) {
+                toast = Toast.makeText(EventForm.this, "Se ha creado el evento", Toast.LENGTH_SHORT);
+
+            } else {
+                toast = Toast.makeText(EventForm.this, "No se ha podido crear", Toast.LENGTH_SHORT);
+            }
+            toast.show();
+        }
+    }
+
+    private class ObtainCalendarTask extends AsyncTask<String, Void, Boolean> {
+        private List<Calendar> calendars;
+
+
+        public ObtainCalendarTask(List<Calendar> calendars) {
+
+            this.calendars = calendars;
+
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            calendars.addAll(calendarExtractor.extractUserCalendars(getApplicationContext()));
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            updateCalendarsSpinner(calendars);
+
+        }
     }
 }
